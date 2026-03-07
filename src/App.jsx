aa5bb64830fa7d7f4c227f645b85a79150977177
colezeu <132004981+colezeu@users.noreply.github.com>
2026-03-07 19:00:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './HomePage.jsx'
 import BalustradeConfiguratorPage from './BalustradeConfiguratorPage.jsx'
 import ShowerConfiguratorPage from './ShowerConfiguratorPage.jsx'
+import WallConfiguratorPage from './WallConfiguratorPage.jsx'
+import DoorConfiguratorPage from './DoorConfiguratorPage.jsx'
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,13 +12,12 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/configurator" element={<HomePage />} />
         <Route path="/configurator/balustrade" element={<BalustradeConfiguratorPage />} />
-        <Route path="/configurator/cabine-dus" element={<ShowerConfiguratorPage />} /> 
-        <Route path="/configurator/cabine-dus" element={<HomePage />} />
+        <Route path="/configurator/cabine-dus" element={<ShowerConfiguratorPage />} />
+        <Route path="/configurator/pereti-cortina" element={<WallConfiguratorPage />} />
+        <Route path="/configurator/usi-sticla" element={<DoorConfiguratorPage />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
-
-
