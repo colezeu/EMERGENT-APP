a7ac0a9cd8f4f4beca70f08df7bf28a394116eab
colezeu <132004981+colezeu@users.noreply.github.com>
2026-03-07 13:25:53 +0200

    Integrate React Router in App component
    Refactor App component to use React Router for navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import HomePage from './HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './HomePage.jsx'
 
 function App() {
-  return <HomePage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} /> {/* Fallback pentru orice altă rută */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
-
-
+export default App
