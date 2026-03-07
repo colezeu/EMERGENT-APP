8b34022bb568ff9c5c50e00ff2c46c12458fa96c
colezeu <132004981+colezeu@users.noreply.github.com>
2026-03-07 14:48:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import './index.css';  // Această linie trebuie să existe!
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'  // ← ACEASTĂ LINIE TREBUIE SĂ EXISTE!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
