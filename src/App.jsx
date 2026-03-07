fd7e11af7a9ec65837dfe40d0c91d56fa581f214
colezeu <132004981+colezeu@users.noreply.github.com>
2026-03-07 13:48:31 +0200

    Add routes for BalustradeConfiguratorPage and HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './HomePage.jsx'
-// import BalustradeConfiguratorPage from './BalustradeConfiguratorPage.jsx'
+import BalustradeConfiguratorPage from './BalustradeConfiguratorPage.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/configurator" element={<BalustradeConfiguratorPage />} /> */}
+        <Route path="/configurator" element={<HomePage />} />
+        <Route path="/configurator/balustrade" element={<BalustradeConfiguratorPage />} />
+        <Route path="/configurator/cabine-dus" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
 export default App
