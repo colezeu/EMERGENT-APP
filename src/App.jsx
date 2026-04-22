ee02ee6c5253b7929f75ef8245bd99798bd92dd7
colezeu <132004981+colezeu@users.noreply.github.com>
2026-04-22 10:23:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import BalustradeConfiguratorPage from './BalustradeConfiguratorPage.jsx'
 import ShowerConfiguratorPage from './ShowerConfiguratorPage.jsx'
 import TerraceConfiguratorPage from './TerraceConfiguratorPage.jsx'
 import PergolaConfiguratorPage from './PergolaConfiguratorPage.jsx'
+import CopertinaConfiguratorPage from './CopertinaConfiguratorPage.jsx'
 import SwingDoorConfiguratorPage from './SwingDoorConfiguratorPage.jsx'
 import SlidingDoorConfiguratorPage from './SlidingDoorConfiguratorPage.jsx'
 import PartitionConfiguratorPage from './PartitionConfiguratorPage.jsx'
@@ -13,20 +14,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/"                              element={<HomePage />} />
-        <Route path="/configurator/balustrade"       element={<BalustradeConfiguratorPage />} />
-        <Route path="/configurator/cabine-dus"       element={<ShowerConfiguratorPage />} />
-        <Route path="/configurator/inchidere-terasa" element={<TerraceConfiguratorPage />} />
-        import CopertinaConfiguratorPage from "./CopertinaConfiguratorPage.jsx";
-
-// Add alongside your existing pergola route:
-<Route path="/configurator/copertina" element={<CopertinaConfiguratorPage />} />
-<Route path="/configurator/pergola"   element={<PergolaConfiguratorPage />} />
-        
-        <Route path="/configurator/usi-batante"      element={<SwingDoorConfiguratorPage />} />
-        <Route path="/configurator/usi-culisante"    element={<SlidingDoorConfiguratorPage />} />
-        <Route path="/configurator/partitionari"     element={<PartitionConfiguratorPage />} />
+        <Route path="/admin"                             element={<AdminPage />} />
+        <Route path="/"                                  element={<HomePage />} />
+        <Route path="/configurator/balustrade"           element={<BalustradeConfiguratorPage />} />
+        <Route path="/configurator/cabine-dus"           element={<ShowerConfiguratorPage />} />
+        <Route path="/configurator/inchidere-terasa"     element={<TerraceConfiguratorPage />} />
+        <Route path="/configurator/pergola"              element={<PergolaConfiguratorPage />} />
+        <Route path="/configurator/copertina"            element={<CopertinaConfiguratorPage />} />
+        <Route path="/configurator/usi-batante"          element={<SwingDoorConfiguratorPage />} />
+        <Route path="/configurator/usi-culisante"        element={<SlidingDoorConfiguratorPage />} />
+        <Route path="/configurator/partitionari"         element={<PartitionConfiguratorPage />} />
       </Routes>
     </BrowserRouter>
   )
